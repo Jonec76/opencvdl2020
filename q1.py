8482b40545c6a09fbf0f4734da609af177f76a7d
jonec76 <jonec76@gmail.com>
2020-11-05 14:35:00 +0800

    Fix the q1 answer

--- q1.py
+++ q1.py
@@ -3,16 +3,21 @@ import numpy as np
 
 img_prefix = 'Dataset_opencvdl/Q1_Image/'
 
-dst = np.ones((680, 373,3), np.uint8)*255
 img = cv2.imread(img_prefix + 'Uncle_Roger.jpg', 1)
-img_resize = cv2.resize(img, (680, 373))                    # Resize image
-img_flip = cv2.flip(img_resize, 1)
-dst = cv2.resize(img, (680, 373))
+height = img.shape[0]
+width = img.shape[1]
+dst = np.ones((width, height,3), np.uint8)*255
+img_flip = cv2.flip(img, 1)
+dst = cv2.resize(img, (width, height))
 
 def load_image():
     img = cv2.imread(img_prefix + 'Uncle_Roger.jpg', 1)
-    img_resize = cv2.resize(img, (680, 373))                    # Resize image
-    cv2.imshow('image',img_resize)
+    
+    print("Height: "+ str(height))
+    print("Width: "+str(width))
+    # img = cv2.resize(img, (680, 373))                    # Resize image
+    # print(img.shape)
+    cv2.imshow('image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -37,20 +42,19 @@ def color_seperation():
 
 def image_flipping():
     img = cv2.imread(img_prefix + 'Uncle_Roger.jpg', 1)
-    img_resize = cv2.resize(img, (680, 373))                    # Resize image
+    # img = cv2.resize(img, (680, 373))                    # Resize image
 
-    img_flip = cv2.flip(img_resize, 1)
-    cv2.imshow('image',img_resize)
+    img_flip = cv2.flip(img, 1)
+    cv2.imshow('image',img)
     cv2.imshow('flip image',img_flip)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 def on_trackbar(x):
-    print(x)
     alpha = x/100
     beta = 1 - alpha
-    cv2.addWeighted(img_resize,alpha,img_flip,beta,0.0,dst)
+    cv2.addWeighted(img,alpha,img_flip,beta,0.0,dst)
     cv2.imshow("image",dst)
 
 def blending():
