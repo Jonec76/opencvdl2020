775ac18667e4d6d58596365455aa1d6e3b284277
jonec76 <jonec9676@gmail.com>
2020-12-25 13:02:09 +0800

    Finish q2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
-import q1
+import q1, q2
 import sys
 
 class GroupBox(QWidget):
@@ -14,6 +14,7 @@ class GroupBox(QWidget):
 		
 		total_widgets = []
 		total_widgets.append(self.c1("1. Image Processing"))
+		total_widgets.append(self.c2("2. Calibration"))
 
 		for idx, widget in enumerate(total_widgets):
 			layout.addWidget(widget, 0, idx)
@@ -25,16 +26,51 @@ class GroupBox(QWidget):
 
 		b1 = QPushButton("1.1 Load image")
 		b2 = QPushButton("1.2 Count coins")
-		x = 5
 		l1 = QLabel(self)
-		# l1.resize(200,100)
 		l1.setText("There are _ conis in coin01.jpg\nThere are _ conis in coin02.jpg")
+		
 		b1.clicked.connect(q1.find_contour)
 		b2.clicked.connect(lambda y: q1.count_coins(l1))
 		
 		vbox.addWidget(b1)
 		vbox.addWidget(b2)
 		vbox.addWidget(l1)
+
+		return groupbox
+
+	def c2(self, title):
+		groupbox = QGroupBox(title)
+		sub_groupbox = QGroupBox("2.3 Find Extrinsic")
+		vbox = QVBoxLayout()
+		groupbox.setLayout(vbox)
+		sub_vbox = QVBoxLayout()
+		sub_groupbox.setLayout(sub_vbox)
+		
+		b1 = QPushButton("2.1 Find Corners")
+		b2 = QPushButton("2.2 find Intrinsic")
+		b4 = QPushButton("2.4 Find Distortion")
+
+		# For sub groupbox
+		label = QLabel("Select image")
+		b3 = QPushButton("2.3 Find Extrinsic")
+		listWidget = QComboBox()
+		
+		for i in range(1, 16):
+			listWidget.addItem(str(i))
+		
+		b1.clicked.connect(q2.find_corners)
+		b2.clicked.connect(q2.find_intrinsic)
+		b3.clicked.connect(lambda y: q2.find_extrinsic(listWidget.currentText()))
+		b4.clicked.connect(q2.find_distortion)
+
+		sub_vbox.addWidget(label)
+		sub_vbox.addWidget(listWidget)
+		sub_vbox.addWidget(b3)
+		vbox.addWidget(b1)
+		vbox.addWidget(b2)
+		vbox.addWidget(sub_groupbox)
+		vbox.addWidget(b4)
+
 
 		return groupbox
 
---
+++ q2.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import glob
+
+img_prefix = 'Datasets/Q2_Image/'
+
+
+def find_corners():
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((11*8,3), np.float32)
+    objp[:,:2] = np.mgrid[0:11,0:8].T.reshape(-1,2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob(img_prefix+'*.bmp')
+    idx = 0
+    for fname in images:
+        img = cv2.imread(fname)
+        # img = cv2.resize(img, (1000, 800), interpolation = cv2.INTER_AREA)
+       
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (11,8), None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners)
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, (11,8), corners2, ret)
+            cv2.namedWindow(str(idx), cv2.WINDOW_NORMAL)
+            cv2.resizeWindow(str(idx), 1000, 800)
+            cv2.imshow(str(idx), img)
+            idx += 1
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    np.savez('output.npz', mtx, dist, rvecs, tvecs)
+    np.savez('img_obj_points.npz', imgpoints, objpoints)
+
+def find_intrinsic(self):
+    with np.load('output.npz') as X:
+        mtx, _, _, _ = [X[i] for i in ('arr_0', 'arr_1', 'arr_2', 'arr_3')]
+
+    print(mtx)
+
+def find_extrinsic(num):
+    with np.load('img_obj_points.npz') as X:
+        imgpoints, objpoints = [X[i] for i in ('arr_0', 'arr_1')]
+    with np.load('output.npz') as X:
+        mtx, dist, _, _ = [X[i]
+                            for i in ('arr_0', 'arr_1', 'arr_2', 'arr_3')]
+
+    num = int(num) - 1
+    retval, rvecs, tvecs = cv2.solvePnP(objpoints[num-1], imgpoints[num-1], mtx, dist)
+    dst, _ = cv2.Rodrigues(rvecs)
+    extrinsic_mtx = cv2.hconcat([dst, tvecs])
+    print(extrinsic_mtx)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def find_distortion():
+    with np.load('output.npz') as X:
+        _, dist, _, _ = [X[i] for i in ('arr_0', 'arr_1', 'arr_2', 'arr_3')]
+
+    print(dist)
